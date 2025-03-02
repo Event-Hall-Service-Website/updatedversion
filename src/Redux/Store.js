99e8084782a593c23d42dbfc59855ac1a6910389
techemmydev <gabrielemmanuelmary12345@gmail.com>
2025-03-02 18:16:38 +0100

    added some new faetures

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import contactReducer from "./slice/ContactSlice";
-
+import subscribeUser from "./slice/subscribersSlice";
 export const store = configureStore({
   reducer: {
     contact: contactReducer,
+    subscribers: subscribeUser,
   },
 });
