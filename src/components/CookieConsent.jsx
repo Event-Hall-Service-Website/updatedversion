6588e11a78a90902c6c1514d8648b1a358da1c68
techemmydev <gabrielemmanuelmary12345@gmail.com>
2025-03-16 02:28:14 +0100

    making correction to my code

--- src/components/CookieConsent.jsx
+++ src/components/CookieConsent.jsx
@@ -48,8 +48,21 @@ const CookieConsent = () => {
             Cookie Preferences
           </DialogTitle>
           <DialogDescription className="text-sm text-gray-600 mt-2 font-plus-jakarta-sans">
-            This website uses cookies to enhance your experience. Accept all or
-            decline?
+            This website stores cookies on your computer. These cookies are used
+            to collect information about how you interact with our website and
+            allow us to remember you. We use this information to improve and
+            customize your browsing experience and for analytics and metrics
+            about our visitors both on this website and other media. To find out
+            more about the cookies we use, see our{" "}
+            <span className="text-blue-600 underline cursor-pointer">
+              Privacy Policy
+            </span>
+            .
+            <br />
+            <br />
+            If you decline, your information will not be tracked when you visit
+            this website. A single cookie will be used in your browser to
+            remember your preference not to be tracked.
           </DialogDescription>
         </DialogHeader>
 
